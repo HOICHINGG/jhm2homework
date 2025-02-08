d7a1ea593571469760aaa5025f35725e9d8894a6
HOICHINGG <s2020046@ptcc.edu.hk>
2025-02-08 10:06:46 +0800

    update

--- Q1/Q1.py
+++ Q1/Q1.py
@@ -1,8 +1,8 @@
-price = input("Enter a price : ")
-another_price = input("Enter another price : ")
-if price == another_price:
+price1 = input("Enter a price : ")
+price2 = input("Enter another price : ")
+if price1 == price2 :
     print("The prices are the same")
-elif price > another_price:
+elif price1 > price2 :
     print("The first price is larger than the second one")
 else:
     print("The first price is smaller than the second one")
